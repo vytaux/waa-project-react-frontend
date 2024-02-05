1ff950d2a1e82a017f8b529d389a4d162e212a83
aakarshan <aakartionsimkhada@gmail.com>
2024-02-05 10:36:52 -0600

    Changed application name to 'NextHome'

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
+import "./App.css";
 import PageRoutes from "./routes/PageRoutes";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
-import {useState} from "react";
+import { useState } from "react";
 
 const App = () => {
+  document.title = "NextHome | #1 Real Estate Platform in the World!";
 
-    document.title = 'OneHouse | #1 Real Estate Platform in the World!';
+  const [currentUser, setCurrentUser] = useState(null);
 
-    const [currentUser, setCurrentUser] = useState(null);
-
-    return (
-        <BrowserRouter>
-            <Header currentUser={currentUser} setCurrentUser={setCurrentUser} />
-            <PageRoutes setCurrentUser={setCurrentUser} />
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Header currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      <PageRoutes setCurrentUser={setCurrentUser} />
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,35 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import hasRole from "../util/hasRole";
 
 const Header = ({ currentUser, setCurrentUser }) => {
+  const logoutHandler = () => {
+    setCurrentUser(null);
+  };
 
-    const logoutHandler = () => {
-        setCurrentUser(null);
-    }
+  return (
+    <header className="header">
+      <nav className="w-full flex">
+        <Link to={"/"} className="title-link">
+          <h1>NextHome</h1>
+        </Link>
 
-    return (
-        <header className='header'>
-            <nav className='w-full flex'>
-                <Link to={'/'} className='title-link'><h1>OneHouse</h1></Link>
+        {hasRole(currentUser, "ROLE_ADMIN") && <h2>This is ADMIN</h2>}
+        {hasRole(currentUser, "ROLE_OWNER") && <h2>This is OWNER</h2>}
+        {hasRole(currentUser, "ROLE_CUSTOMER") && <h2>This is CUSTOMER</h2>}
 
-                {hasRole(currentUser, 'ROLE_ADMIN') && <h2>This is ADMIN</h2>}
-                {hasRole(currentUser, 'ROLE_OWNER') && <h2>This is OWNER</h2>}
-                {hasRole(currentUser, 'ROLE_CUSTOMER') && <h2>This is CUSTOMER</h2>}
-
-                {!currentUser
-                    ? <Link to='/login' className='login-logout-link'>Login</Link>
-                    : <Link className='login-logout-link' onClick={logoutHandler}>Logout</Link>
-                }
-            </nav>
-        </header>
-    );
-}
-
+        {!currentUser ? (
+          <Link to="/login" className="login-logout-link">
+            Login
+          </Link>
+        ) : (
+          <Link className="login-logout-link" onClick={logoutHandler}>
+            Logout
+          </Link>
+        )}
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
