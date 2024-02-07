7f5f8cc32c6b11a7f5f8840fa6afe1f0d3b7d876
Vytautas A <vytautasasm@gmail.com>
2024-02-07 10:52:38 -0600

    register fix

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,33 +3,17 @@ import FetchService from '../service/FetchService';
 import extractJwtPayload from '../util/extractJwtPayload';
 import { useNavigate } from 'react-router-dom';
 
-function Register({ setCurrentUser }) {
+function Register() {
     const email = useRef();
     const password = useRef();
     const role = useRef();
     const navigate = useNavigate();
 
     const register = () => {
-        // alert("Registering user")
-        console.log(email.current.value)
-        console.log(password.current.value)
-        console.log(role.current.value)
 
-        // FetchService.login(email.current.value, password.current.value, role.current.value)
-        //     .then(response => {
-        //         const payload = extractJwtPayload(response.data.accessToken);
-
-        //         console.log(payload)
-        //         const currentUser = {
-        //             email: payload.sub,
-        //             roles: payload.roles
-        //         };
-        //         setCurrentUser(currentUser);
-
-        //         navigate('/')
-        //     });
-
-        navigate('/')
+        // TODO some kind of confirmation
+        FetchService.register(email.current.value, password.current.value, role.current.value)
+            .then(response => navigate('/'));
 
     }
 
@@ -48,11 +32,11 @@ function Register({ setCurrentUser }) {
                     <label htmlFor="roleDropdown">Select an option: </label>
                     <select id="roleDropdown" ref={role}>
                         <option value="">-- Select an option--</option>
-                        <option value="ROLE_OWNER">Home Owner</option>
-                        <option value="ROLE_CUSTOMER">Home Buyer</option>
+                        <option value="OWNER">Home Owner</option>
+                        <option value="CUSTOMER">Home Buyer</option>
                     </select>
                 </div>
-                <button type="submit" className="btn btn-primary">Register</button>
+                <button type="submit" className="btn btn-primary" onClick={register}>Register</button>
             </div>
         </main>
     );
