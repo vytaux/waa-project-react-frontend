c918bc3fa31fba2fc01a516348e8849775481f7e
aakarshan <aakartionsimkhada@gmail.com>
2024-02-08 16:44:23 -0600

    Save button is removed, only logged in user can save the property

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,39 +1,37 @@
-import React, {useContext} from "react";
 import formatMoney from "../util/formatMoney";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import randomPictureProvider from "../util/randomPictureProvider";
 import propertyStatusMapper from "../util/propertyStatusMapper";
-import FetchService from "../service/FetchService";
-import UserContext from "../context/UserContext";
+import SaveProperty from "./SaveProperty";
 
 const Property = ({ property }) => {
+  const pictureUrl = randomPictureProvider();
+  const propertyStatus = propertyStatusMapper(property.status);
 
-    const currentUser = useContext(UserContext);
-
-    const pictureUrl = randomPictureProvider();
-    const propertyStatus = propertyStatusMapper(property.status);
-
-    const handleAddToSavedPropertiesList = () => {
-        FetchService.addToSavedPropertiesList(currentUser.accessToken, property.id)
-            .then(response => console.log(response));
-    }
-
-    return (
-        <div className='property'>
-            <Link to={`/properties/${property.slug}`} key={property.id} className='picture-link'>
-                <div className={`status-badge ${propertyStatus}`}>{propertyStatus}</div>
-                <img className='picture' src={pictureUrl} alt=""/>
-            </Link>
-            <div className="info">
-                <Link to={`/properties/${property.slug}`} key={property.id} className='name'>
-                    {property.name}
-                </Link>
-                <div className='price'>{formatMoney(property.price)}</div>
-                <div className='description'>{property.description}</div>
-                <button onClick={handleAddToSavedPropertiesList}>Save</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="property">
+      <Link
+        to={`/properties/${property.slug}`}
+        key={property.id}
+        className="picture-link"
+      >
+        <div className={`status-badge ${propertyStatus}`}>{propertyStatus}</div>
+        <img className="picture" src={pictureUrl} alt="" />
+      </Link>
+      <div className="info">
+        <Link
+          to={`/properties/${property.slug}`}
+          key={property.id}
+          className="name"
+        >
+          {property.name}
+        </Link>
+        <div className="price">{formatMoney(property.price)}</div>
+        <div className="description">{property.description}</div>
+        <SaveProperty property={property} />
+      </div>
+    </div>
+  );
+};
 
 export default Property;
