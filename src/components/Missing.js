b1e6c9ade9681aacf597d9fdabefa873b0b1f21c
Vytautas A <vytautasasm@gmail.com>
2024-02-07 19:59:32 -0600

    better looking 404

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,12 +1,18 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
+import './Missing.css';
 
 const Missing = () => {
 
-    return <div>
-        <h1> Sorry page not found... 404 </h1>
-    </div>
-
+    return (
+        <div className="not-found-page">
+            <div className="content">
+                <h1>Oops!</h1>
+                <h2>Sorry, the page you're looking for cannot be found...</h2>
+                <Link to='/'>Go Back to Homepage</Link>
+            </div>
+        </div>
+    )
 }
 
 export default Missing;
